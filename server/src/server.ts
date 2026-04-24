09c4b9fc2251c3f496e4e4165d379f71cf5b577f
Eduardo de Morais Tajra <eduardotajra@edu.unifor.br>
2026-04-24 04:44:02 -0300

    debug: try tv_embedded+mweb player clients to bypass bot detection

--- server/src/server.ts
+++ server/src/server.ts
@@ -60,8 +60,12 @@ async function main() {
       fsd.writeFileSync(cookiesPath, Buffer.from(cookiesB64, 'base64').toString('utf8').replace(/\r\n/g, '\n'))
     }
 
-    const jsRuntime = process.platform !== 'win32' ? 'nodejs:/usr/local/bin/node' : 'nodejs'
-    const testArgs: string[] = ['--ignore-config', '--no-warnings', '--js-runtimes', jsRuntime, '--format', '18/bestaudio/best', '--output', pathd.join(os.tmpdir(), `debug-${videoId}.mp4`)]
+    const testArgs: string[] = [
+      '--ignore-config', '--no-warnings',
+      '--extractor-args', 'youtube:player_client=tv_embedded,mweb',
+      '--format', '18/bestaudio/best',
+      '--output', pathd.join(os.tmpdir(), `debug-${videoId}.mp4`),
+    ]
     if (cookiesPath) { testArgs.push('--cookies', cookiesPath) }
     testArgs.push(url)
     app.log.info({ cookiesPath, cookiesSet: !!cookiesB64, cookiesLen: cookiesB64?.length }, 'debug download args')
